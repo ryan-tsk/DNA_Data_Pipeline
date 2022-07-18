27a697fed591b2bb2bf23fe72d8b1f895b836dc6
Ryan Tan <rst818@imperial.ac.uk>
2022-07-18 11:57:16 +0800

    Updated testCLI.py to test for dependency issues between bonito, scrappy and turbodna

--- testCLI.py
+++ testCLI.py
@@ -1,7 +1,7 @@
-from node import NodeCLI
+from node import NodeCLICommand
 
 command = 'bonito basecaller --batchsize 32 dna_r10.4_e8.1_sup@v3.4 bonito-test/ > bonito-test/output.fastq'
-test = NodeCLI(command, input_path='', output_path='')
+test = NodeCLICommand('bonito', 'bonito-test/output.fastq', command)
 test.process()
 
 
