b51f070d6ccfb81eac60e8eda4abb9c1a3abd9dc
Ryan Tan <rst818@imperial.ac.uk>
2022-07-20 16:25:08 +0100

    crreated new test for stage 3 (alignment)

--- testCLI.py
+++ testCLI.py
@@ -1,5 +1,13 @@
-from node import NodeCLICommand
+from node import NodeCLICommand, NodeFunction, NodeFactory
+import yaml
 
-command = 'bonito basecaller --batchsize 32 --fastq --weights 1 dna_r9.4.1_e8.1_sup@v3.3 test_results/stage2/ > test_results/stage2/output.fastq'
-test = NodeCLICommand('bonito', 'test_results/stage2/output.fastq', command)
-test.process()
+# command = 'bonito basecaller --batchsize 32 --fastq --weights 1 dna_r9.4.1_e8.1_sup@v3.3 test_results/stage2/ > test_results/stage2/output.fastq'
+# test = NodeCLICommand('bonito', 'test_results/stage2/output.fastq', command)
+# test.process()
+
+with open('config/config_stage3.yaml', 'r') as configFile:
+    config = yaml.load(configFile, Loader=yaml.FullLoader)
+
+factory = NodeFactory()
+node = NodeFactory.create_node(config['Alignment'], 'Alignment')
+node.process('test_results/stage2/output.fastq')
