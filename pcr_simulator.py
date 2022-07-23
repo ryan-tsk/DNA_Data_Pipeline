e6eb9c5e3623bb3b48bc59dc8dd51a11c3213831
Ryan Tan <rst818@imperial.ac.uk>
2022-07-23 10:17:58 +0100

    ammended pcr_simulator (clustalw cannot read multiple sequences with the same id)

--- pcr_simulator.py
+++ pcr_simulator.py
@@ -44,10 +44,10 @@ def create_weighted_fasta(bc_path, gt_path, directory: str, weight: int, prefix_
     for i, bc_seq in enumerate(bc_seqs):
         output = []
         seq_id = f'{prefix_id}_{i}'
-        output.extend([f'>{seq_id}', bc_seq])
+        output.extend([f'>{seq_id}_{i}', bc_seq])
 
         for j in range(weight):
-            output.extend([f'>{seq_id}', gt_seqs[i]])
+            output.extend([f'>{seq_id}_{j+1}', gt_seqs[i]])
 
         filepath = os.path.join(directory, f'{seq_id}.fasta')
         with open(filepath, 'w') as fasta:
