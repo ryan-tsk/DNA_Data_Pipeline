5fed5ea0bfc29f15275635261850dee6c095213a
Ryan Tan <rst818@imperial.ac.uk>
2022-07-26 16:44:08 +0100

    added sig.parameters to if statement in node process

--- node.py
+++ node.py
@@ -38,7 +38,7 @@ class NodeFunction(Node):
     def process(self, input_stream, result_directory):
         print(self.name + ' node is processing...')
         sig = signature(self.data_process)
-        if 'result_dictionary' in sig:
+        if 'result_dictionary' in sig.parameters:
             print("testing..")
             output = self.data_process(input_stream, result_directory, **self.variables)
             return output
